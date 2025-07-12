50f8f38e035e417c7270624143add05ab2ddab0c
Recker-Dev <reckerdev@gmail.com>
2025-07-13 02:22:36 +0530

    Changed reload stat to False for uvicorn

--- backend/vector-micro-service/app/main.py
+++ backend/vector-micro-service/app/main.py
@@ -10,4 +10,4 @@ app.include_router(health_router, prefix="/health", tags=["Health Check"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=False)
